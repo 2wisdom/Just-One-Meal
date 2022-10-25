6933c2d4073af06cc7766d0039e632c2c959a88b
Lizzie <leejihye7117@gmail.com>
2022-10-25 23:20:08 +0900

    PORT 복구

--- back/src/config/config.ts
+++ back/src/config/config.ts
@@ -7,6 +7,6 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
 export const MONGODB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/`;
-export const SERVER_PORT = process.env.PORT
+export const SERVER_PORT = process.env.SERVER_PORT
   ? Number(process.env.SERVER_PORT)
   : 1337;
