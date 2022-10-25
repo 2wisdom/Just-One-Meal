cb8abee6f037163dae640846ba3963791081b3f0
Lizzie <leejihye7117@gmail.com>
2022-10-25 23:23:08 +0900

    PORT 수정

--- back/src/config/config.ts
+++ back/src/config/config.ts
@@ -7,6 +7,4 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
 export const MONGODB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/`;
-export const SERVER_PORT = process.env.PORT
-  ? Number(process.env.SERVER_PORT)
-  : 1337;
+export const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 1337;
